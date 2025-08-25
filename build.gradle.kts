b8a244d5ca16238f204663707ed3abf295fd765d
Dexton Anderson <danderson@ihmc.org>
2025-08-25 15:43:23 -0500

    Pin to ihmc-open-robotics-software release and disable cron job for slow workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,18 +15,18 @@ ihmc {
 mainDependencies {
    api("org.apache.xmlgraphics:batik-dom:1.14")
 
-   api("us.ihmc:ihmc-avatar-interfaces-visualizers:source")
-   api("us.ihmc:robotiq-hand-drivers:source")
-   api("us.ihmc:ihmc-model-file-loader:source")
-   api("us.ihmc:ihmc-manipulation-planning:source")
+   api("us.ihmc:ihmc-avatar-interfaces-visualizers:0.14.0-250815")
+   api("us.ihmc:robotiq-hand-drivers:0.14.0-250815")
+   api("us.ihmc:ihmc-model-file-loader:0.14.0-250815")
+   api("us.ihmc:ihmc-manipulation-planning:0.14.0-250815")
    api("us.ihmc:ihmc-parameter-tuner:0.15.3")
-   api("us.ihmc:ihmc-footstep-planning-visualizers:source")
-   api("us.ihmc:ihmc-high-level-behaviors:source")
+   api("us.ihmc:ihmc-footstep-planning-visualizers:0.14.0-250815")
+   api("us.ihmc:ihmc-high-level-behaviors:0.14.0-250815")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-avatar-interfaces-test:source")
-   api("us.ihmc:ihmc-sensor-processing-test:source")
-   api("us.ihmc:ihmc-simulation-toolkit-test:source")
+   api("us.ihmc:ihmc-avatar-interfaces-test:0.14.0-250815")
+   api("us.ihmc:ihmc-sensor-processing-test:0.14.0-250815")
+   api("us.ihmc:ihmc-simulation-toolkit-test:0.14.0-250815")
    api("us.ihmc:ihmc-messager-test:0.2.1")
 }
